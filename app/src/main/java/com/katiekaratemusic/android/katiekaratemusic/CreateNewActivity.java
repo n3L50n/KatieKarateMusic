8d8cc2eb2c4e97b8ecfd82ef71ffecfc6661ecfc
Nelson Wells <nelsonandrewwells@gmail.com>
2017-03-18 22:07:41 -0700

    Add system back button to Record activity.

--- app/src/main/java/com/katiekaratemusic/android/katiekaratemusic/CreateNewActivity.java
+++ app/src/main/java/com/katiekaratemusic/android/katiekaratemusic/CreateNewActivity.java
@@ -1,7 +1,10 @@
 package com.katiekaratemusic.android.katiekaratemusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CreateNewActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class CreateNewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new);
+
+        ImageButton backButton = (ImageButton) findViewById(R.id.create_new_system_back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backIntent = new Intent(CreateNewActivity.this, MainActivity.class);
+                startActivity(backIntent);
+            }
+        });
     }
 }
